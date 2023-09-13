cc747284c5fb3bef0c3ae91251b2d66eca8429f0
al-bratkov <al.bratkov15@gmail.com>
2023-09-13 21:41:32 +0300

    trying the new branch

--- describe_module.py
+++ describe_module.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 
 def the_biggest(df):
@@ -23,4 +26,39 @@ def the_biggest(df):
 
 
 def find_outliers(df):
+    # find price outliers
+    iqr = df["price"].quantile(0.75) - df["price"].quantile(0.25)
+    whisker_h = df["price"].quantile(0.75) + 1.5 * iqr
+    whisker_l = df["price"].quantile(0.25) - 1.5 * iqr
+    out_price = df[(df["price"] > whisker_h) | (df["price"] < whisker_l)]
+    if out_price.shape[0] > 15:
+        print("There are more than 15 cars having too high or low price. Check the dataframe manually")
+    else:
+        for i in out_price.index:
+            print(f"Enter 1 if {df.loc[i, 'brand']} {df.loc[i, 'model']} for {df.loc[i, 'price']} is not right note. Enter any other key if it is")
+            print(df.loc[i, 'link'])
+            inpt = input()
+            if inpt == '1':
+                df.drop(i, inplace=True)
+
+    iqr = df["mileage"].quantile(0.75) - df["mileage"].quantile(0.25)
+    whisker_h = df["mileage"].quantile(0.75) + 1.5 * iqr
+    whisker_l = df["mileage"].quantile(0.25) - 1.5 * iqr
+    out_miles = df[(df["mileage"] > whisker_h) | (df["mileage"] < whisker_l)]
+    if out_miles.shape[0] > 15:
+        print("There are more than 15 cars having too high or low mileage. Check the dataframe manually")
+    else:
+        for i in out_miles.index:
+            print(f"Enter 1 if {df.loc[i, 'brand']} {df.loc[i, 'model']} for {df.loc[i, 'price']} is not right note. Enter any other key if it is")
+            print(df.loc[i, 'link'])
+            inpt = input()
+            if inpt == '1':
+                df.drop(i, inplace=True)
+
+
+def show_distribution(df):
+    print("Let's look at distribution of quantitative data: price, mileage, age and horsepower")
+    fig, ax = plt.subplots(2, 2)
+
+
 
